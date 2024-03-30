10fc87907295ccc6743b0cb6a37be8e1be0fbdfe
Timothy <nathan.moritz@duocore.dev>
2024-03-30 16:26:49 -0400

    Refactored the images and layouts

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -12,11 +12,8 @@ import { HighSeasMaterialModule } from './material-module';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
-// /assets/es-cartoon.jpg
-
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
   standalone: true,
   imports: [
     HomeComponent,
@@ -32,48 +29,53 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
   providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } }, MovieService, HttpClientModule,],
   styleUrls: ['./app.component.sass', '../styles.sass'],
   template: `
+    <div class="background-image"></div>
     <main class="earth-spirit">
       <header class="brand-name">
         <div class="container-fluid">
-          <a [routerLink]="['/']">
-            <img id="the-spirits" src="/assets/home-icon.jpg" height=150px width=150px alt="logo" aria-hidden="true">
-          </a>
-          <button mat-button [matMenuTriggerFor]="movies" class="navbar-links big-btn">Movies</button>
-          <button mat-button [matMenuTriggerFor]="shows" class="navbar-links big-btn">Shows</button>
-
-          <mat-menu #movies="matMenu">
-            <button mat-menu-item [matMenuTriggerFor]="newupcomingtopratedpopularmovies">Now Playing / Popular / Top Rated / Upcoming Movies</button>
-            <button mat-menu-item [matMenuTriggerFor]="discoversearchmovies">Discover & Search Movies</button>
-          </mat-menu>
-
-          <mat-menu #shows="matMenu">
-            <button mat-menu-item [matMenuTriggerFor]="newupcomingtopratedpopularshows">Now Playing / Popular / Top Rated / Upcoming Shows</button>
-            <button mat-menu-item [matMenuTriggerFor]="discoversearchshows">Discover & Search Shows</button>
-          </mat-menu>
-
-          <mat-menu #newupcomingtopratedpopularmovies="matMenu">
-            <button mat-menu-item> <a [routerLink]="['now-playing/movies']">Now Playing Movies</a></button>
-            <button mat-menu-item><a [routerLink]="['popular/movies']">Popular Movies</a></button>
-            <button mat-menu-item><a [routerLink]="['top-rated/movies']">Top Rated Movies</a></button>
-            <button mat-menu-item><a [routerLink]="['upcoming/movies']">Upcoming Movies</a></button>
-          </mat-menu>
-
-          <mat-menu #discoversearchmovies="matMenu">
-            <button mat-menu-item><a [routerLink]="['discover/movies']">Discover Movies</a></button>
-            <button mat-menu-item><a [routerLink]="['search/movies']">Search Movies</a></button>
-          </mat-menu>
-
-          <mat-menu #newupcomingtopratedpopularshows="matMenu">
-            <button mat-menu-item><a [routerLink]="['airing-today/shows']">Airing Today Shows</a></button>
-            <button mat-menu-item><a [routerLink]="['popular/shows']">Popular Shows</a></button>
-            <button mat-menu-item><a [routerLink]="['top-rated/shows']">Top Rated Shows</a></button>
-            <button mat-menu-item><a [routerLink]="['on-the-air/shows']">On The Air Shows</a></button>
-          </mat-menu>
-
-          <mat-menu #discoversearchshows="matMenu">
-            <button mat-menu-item><a [routerLink]="['discover/shows']">Discover Shows</a></button>
-            <button mat-menu-item><a [routerLink]="['search/shows']">Search Shows</a></button>
-          </mat-menu>
+          <nav class="dota-navbar">
+            <div class="navbar-content">
+              <a [routerLink]="['/']">
+                <img id="big-titty-waifu-svg" src="/assets/home-icon.jpg" height="50" width="50" alt="logo" aria-hidden="true">
+              </a>
+              <div class="menu-wrapper">
+                <div class="left-menu">
+                  <div class="dropdown">
+                    <a class="dropdown-toggle">Now Playing / Popular / Top Rated / Upcoming Movies</a>
+                    <div class="dropdown-menu">
+                      <a [routerLink]="['now-playing/movies']">Now Playing Movies</a>
+                      <a [routerLink]="['popular/movies']">Popular Movies</a>
+                      <a [routerLink]="['top-rated/movies']">Top Rated Movies</a>
+                      <a [routerLink]="['upcoming/movies']">Upcoming Movies</a>
+                    </div>
+                  </div>
+                  <div class="dropdown">
+                    <a class="dropdown-toggle">Discover & Search Movies</a>
+                    <div class="dropdown-menu">
+                      <a [routerLink]="['discover/movies']">Discover Movies</a>
+                      <a [routerLink]="['search/movies']">Search Movies</a>
+                    </div>
+                  </div>
+                  <div class="dropdown">
+                    <a class="dropdown-toggle">Airing Today / Popular / Top Rated / On The Air Shows</a>
+                    <div class="dropdown-menu">
+                      <a [routerLink]="['airing-today/shows']">Airing Today Shows</a>
+                      <a [routerLink]="['popular/shows']">Popular Shows</a>
+                      <a [routerLink]="['top-rated/shows']">Top Rated Shows</a>
+                      <a [routerLink]="['on-the-air/shows']">On The Air Shows</a>
+                    </div>
+                  </div>
+                  <div class="dropdown">
+                    <a class="dropdown-toggle">Discover & Search Shows</a>
+                    <div class="dropdown-menu">
+                      <a [routerLink]="['discover/shows']">Discover Shows</a>
+                      <a [routerLink]="['popular/movies']">Popular Movies</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
         </div>
       </header>
       <section class="content the-girls">
