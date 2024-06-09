ae2991a87067c8ff78e5bc5aa41199f5a7ca6360
Timothy <nathan.moritz@duocore.dev>
2024-06-08 21:06:26 -0400

    Added sass so that the page doesnt scroll to the left and right

--- web/src/app/home/home.component.ts
+++ web/src/app/home/home.component.ts
@@ -9,19 +9,19 @@ import { TvShowService } from '../tv-service.service';
   selector: 'app-home',
   template: `
     <body>
-      <div class="movie-poster">
-        <img src="/assets/the-spirits.jpg" alt="Movie Poster" (animationend)="onAnimationEnd()">
-      </div>
-      <div class="star-wars-text">
-        <p>
-          In a land far far away a engineer had a vision for an application to find new shows and movies then obtain them in an automated way. That applcation is High Seas.
-        </p>
-      </div>
-      <div class="application-text">
-        <p>
-          Welcome to High Seas.
-        </p>
-      </div>
+    <div class="movie-poster">
+      <img src="/assets/the-spirits.jpg" alt="Movie Poster" (animationend)="onAnimationEnd()">
+    </div>
+    <div class="star-wars-text">
+      <p>
+        In a land far far away a engineer had a vision for an application to find new shows and movies then obtain them in an automated way. That applcation is High Seas.
+      </p>
+    </div>
+    <div class="application-text">
+      <p>
+        Welcome to High Seas.
+      </p>
+    </div>
     </body>
   `,
   standalone: true,
@@ -31,9 +31,7 @@ import { TvShowService } from '../tv-service.service';
 })
 export class HomeComponent implements OnInit {
   constructor() {}
-
   ngOnInit(): void {}
-
   onAnimationEnd() {
     const starWarsText = document.querySelector('.star-wars-text');
     const applicationText = document.querySelector('.application-text');
