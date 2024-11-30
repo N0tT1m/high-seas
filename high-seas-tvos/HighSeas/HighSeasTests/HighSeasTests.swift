c783f6db29683dac2972272fbb6d9032e4d79b59
Nathan Moritz <nathan.moritz@duocore.dev>
2024-11-30 15:57:38 -0500

    Added mobile app.

---
+++ high-seas-tvos/HighSeas/HighSeasTests/HighSeasTests.swift
@@ -0,0 +1,17 @@
+//
+//  HighSeasTests.swift
+//  HighSeasTests
+//
+//  Created by Nathan Moritz on 11/30/24.
+//
+
+import Testing
+@testable import HighSeas
+
+struct HighSeasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ high_seas_mobile/android/app/src/main/kotlin/dev/duocore/high_seas_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.duocore.high_seas_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
