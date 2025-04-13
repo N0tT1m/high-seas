04f07880abc47052644e18550b6d1321591e736b
Timothy <nathan.moritz@duocore.dev>
2025-04-13 18:05:14 -0400

    Added sumo logic http traces

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,9 +1,12 @@
 package utils
 
 import (
+	"io"
 	"log"
+	"net/http"
 	"net/http/cookiejar"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -19,6 +22,7 @@ func EnvVar(key string, defaultVal string) string {
 	return value
 }
 
+// CreateCookieJar creates a new cookie jar
 func CreateCookieJar() *cookiejar.Jar {
 	jar, err := cookiejar.New(nil)
 	if err != nil {
@@ -26,4 +30,28 @@ func CreateCookieJar() *cookiejar.Jar {
 	}
 
 	return jar
+}
+
+// CreateHTTPClient creates a new HTTP client with timeout and cookie jar
+func CreateHTTPClient() *http.Client {
+	jar := CreateCookieJar()
+	return &http.Client{
+		Jar:     jar,
+		Timeout: 30 * time.Second,
+	}
+}
+
+// CreatePostRequest creates a new HTTP POST request with the given content type and body
+func CreatePostRequest(url, contentType string, body io.Reader) (*http.Request, error) {
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", contentType)
+
+	// Add any common headers here
+	req.Header.Set("User-Agent", "High-Seas/1.0")
+
+	return req, nil
 }
